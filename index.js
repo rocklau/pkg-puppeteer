b3dfeb18617e903805f3ac1c1dd953bda005c979
liuyan <liuyan@x-y-t.cn>
2018-09-11 09:48:32 +0800

    init code

---
+++ index.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const puppeteer = require('puppeteer');
+
+const isPkg = typeof process.pkg !== 'undefined';
+
+//mac path replace
+const chromiumExecutablePath = (isPkg ?
+  puppeteer.executablePath().replace(
+    /^.*?\/node_modules\/puppeteer\/\.local-chromium/,
+    path.join(path.dirname(process.execPath), 'chromium')
+  ) :
+  puppeteer.executablePath()
+);
+
+console.log(process.platform)
+//check win32
+if (process.platform == 'win32') {
+  chromiumExecutablePath = (isPkg ?
+    puppeteer.executablePath().replace(
+      /^.*?\\node_modules\\puppeteer\\\.local-chromium/,
+      path.join(path.dirname(process.execPath), 'chromium')
+    ) :
+    puppeteer.executablePath()
+  );
+}
+
+(async () => {
+
+  const browser = await puppeteer.launch({
+    executablePath: chromiumExecutablePath,
+    headless: false
+  });
+
+  const page = await browser.newPage()
+
+  await page.goto('https://www.google.com')
+
+})()
